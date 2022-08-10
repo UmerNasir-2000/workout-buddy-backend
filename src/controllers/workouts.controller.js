c0f09aa5c04aa8e2e775fb7674077c1304568c1c
Umer Nasir <umer.nas@outlook.com>
2022-08-10 21:07:21 +0500

    Added GET, PATCH & DELETE Requests for Workouts

--- src/controllers/workouts.controller.js
+++ src/controllers/workouts.controller.js
@@ -1,4 +1,5 @@
 const { StatusCodes } = require("http-status-codes");
+const mongoose = require("mongoose");
 const Workouts = require("../models/workouts.model");
 
 exports.fetchWorkouts = async (req, res) => {
@@ -6,8 +7,56 @@ exports.fetchWorkouts = async (req, res) => {
   res.status(StatusCodes.OK).json({ message: "GET /api/workouts", workouts });
 };
 
+exports.fetchWorkoutById = async (req, res) => {
+  const { id } = req.params;
+  const workout = await Workouts.findById(id);
+
+  if (!mongoose.isValidObjectId(id))
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ error: `Invalid MongoDB Id.` });
+
+  if (!workout)
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ error: `workout with id = ${id} does not exist` });
+
+  res
+    .status(StatusCodes.OK)
+    .json({ message: `GET /api/workouts/${id}`, workout });
+};
+
 exports.createWorkout = async (req, res) => {
   const { title, reps, weight } = req.body;
   const workout = await Workouts.create({ title, reps, weight });
   res.status(StatusCodes.OK).json({ message: "POST /api/workouts", workout });
+};
+
+exports.removeWorkout = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const workout = await Workouts.findByIdAndDelete(id);
+    return res
+      .status(StatusCodes.OK)
+      .json({ message: `DELETE /api/workouts/${id}`, workout });
+  } catch (error) {
+    console.log("error :>> ", error);
+  }
+};
+
+exports.updateWorkout = async (req, res) => {
+  const { title, reps, weight } = req.body;
+  const { id } = req.params;
+  try {
+    const workout = await Workouts.findByIdAndUpdate(id, {
+      title,
+      reps,
+      weight,
+    });
+    return res
+      .status(StatusCodes.OK)
+      .json({ message: `PATCH /api/workouts/${id}`, workout });
+  } catch (error) {
+    console.log("error :>> ", error);
+  }
 };
--- src/routes/workouts.route.js
+++ src/routes/workouts.route.js
@@ -2,12 +2,19 @@ const express = require("express");
 const {
   fetchWorkouts,
   createWorkout,
+  fetchWorkoutById,
+  removeWorkout,
+  updateWorkout,
 } = require("../controllers/workouts.controller");
 
 const router = express.Router();
 
 router.route("/").get(fetchWorkouts).post(createWorkout);
 
-router.route("/:id").get().patch().delete();
+router
+  .route("/:id")
+  .get(fetchWorkoutById)
+  .patch(updateWorkout)
+  .delete(removeWorkout);
 
 module.exports = router;
